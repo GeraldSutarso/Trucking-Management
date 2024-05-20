9367fe2cc7942ab26ec073783f8a6e36a2f6d34a
Jonah0705 <jonahpupella@gmail.com>
2024-05-20 20:37:37 +0700

    [ADD] Additonal page for maintenance
    Additional Page for maintenance, no styling yet

--- tm/maintenance/forms.py
+++ tm/maintenance/forms.py
@@ -1,4 +1,20 @@
 from django import forms
 
 class ExcelUploadForm(forms.Form):
-    excel_file = forms.FileField(label="Insert your data here")
+    excel_file = forms.FileField(label='Insert your data here')
+
+
+class ManualInputForm(forms.Form):
+    ALTITUDE = forms.FloatField(label='Altitude')
+    ENGINE_LOAD = forms.FloatField(label='Engine Load')
+    BAROMETRIC_PRESSURE = forms.FloatField(label='Barometric Pressure')
+    ENGINE_COOLANT_TEMP = forms.FloatField(label='Engine Coolant Temperature')
+    AMBIENT_AIR_TEMP = forms.FloatField(label='Ambient Air Temperature')
+    ENGINE_RPM = forms.FloatField(label='Engine RPM')
+    INTAKE_MANIFOLD_PRESSURE = forms.FloatField(label='Intake Manifold Pressure')
+    MAF = forms.FloatField(label='MAF')
+    AIR_INTAKE_TEMP = forms.FloatField(label='Air Intake Temperature')
+    SPEED = forms.FloatField(label='Speed')
+    THROTTLE_POS = forms.FloatField(label='Throttle Position')
+    ENGINE_RUNTIME = forms.CharField(label='Engine Runtime (HH:MM:SS)')
+    truck_select = forms.ChoiceField(choices=[('truck1', 'Truck 1'), ('truck2', 'Truck 2')], label='Select Truck')
--- tm/maintenance/views.py
+++ tm/maintenance/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from django.http import JsonResponse
-from .forms import ExcelUploadForm
+from .forms import ExcelUploadForm, ManualInputForm
 from .models import ExcelData, UploadedFile, ColumnAverages
 import pandas as pd
 import numpy as np
@@ -11,7 +11,15 @@ from django.conf import settings
 import os
 from django.http import FileResponse
 from django.conf import settings
+from accounts.models import User, Driver, Customer
+from django.contrib.auth.decorators import login_required, user_passes_test
 
+def im_customer(user):
+    return user.is_customer
+def im_superman(user):
+    return user.is_superuser
+def im_driver(user):
+    return user.is_driver
 
 def load_data(file_path):
     df = pd.read_excel(file_path)
@@ -26,8 +34,9 @@ def preprocess_data(df):
     df_preprocessed = df[columns_to_keep].copy()
     df_preprocessed['ENGINE_RUNTIME'] = df_preprocessed['ENGINE_RUNTIME'].fillna('00:00:00')
     df_preprocessed['ENGINE_RUNTIME'] = df_preprocessed['ENGINE_RUNTIME'].apply(lambda x: datetime.strptime(str(x), "%H:%M:%S"))
+    print(df['ENGINE_RUNTIME'])
     df_preprocessed['ENGINE_RUNTIME'] = df_preprocessed['ENGINE_RUNTIME'].dt.hour * 3600 + df_preprocessed['ENGINE_RUNTIME'].dt.minute * 60 + df_preprocessed['ENGINE_RUNTIME'].dt.second
-    
+    print("After rmoving seconds", df['ENGINE_RUNTIME'])
     for column in columns_to_keep:
         df_preprocessed[column] = df_preprocessed[column].astype(str).str.extract(r'(\d+\.?\d*)').astype(float)
         
@@ -73,21 +82,29 @@ def print_anomaly_scores(isolation_forest, df, numerical_columns):
     for score in anomaly_scores:
         print(score)
 
+
+@login_required(login_url='login')
+@user_passes_test(im_driver, login_url='/login')
 def upload_display_excel(request):
     data_to_display = None
     error_message = None
     column_averages = None
     maintenance_status = None
+    truck_select = 'truck1'  # Default selection
 
     if request.method == 'POST':
         form = ExcelUploadForm(request.POST, request.FILES)
+        truck_select = request.POST.get('truck_select', 'truck1')
         if form.is_valid():
             try:
                 excel_file = request.FILES['excel_file']
                 new_upload = UploadedFile(file=excel_file)
                 new_upload.save()
 
-                train_file_path = os.path.join(settings.MEDIA_ROOT, 'training_data', 'exp2_19drivers_1car_1route.xlsx')
+                if truck_select == 'truck2':
+                    train_file_path = os.path.join(settings.MEDIA_ROOT, 'training_data', 'badData.xlsx')
+                else:
+                    train_file_path = os.path.join(settings.MEDIA_ROOT, 'training_data', 'exp2_19drivers_1car_1route.xlsx')
                 train_df = load_data(train_file_path)
                 train_preprocessed, numerical_columns_updated = preprocess_data(train_df)
                 train_imputed = handle_missing_values(train_preprocessed, numerical_columns_updated)
@@ -108,7 +125,7 @@ def upload_display_excel(request):
 
                 non_outlier_rows = df.shape[0] - len(outlier_rows)
                 outlier_percentage = len(outlier_rows) / len(df) * 100
-
+                print(total_anomaly_score)
                 if total_anomaly_score <= -2:
                     maintenance_status = "The vehicle desperately needs maintenance. Status: Severe"
                 elif total_anomaly_score <= 0:
@@ -147,6 +164,7 @@ def upload_display_excel(request):
         'error_message': error_message,
         'column_averages': column_averages,
         'maintenance_status': maintenance_status,
+        'truck_select': truck_select  # Add this line to pass the selected truck type
     }
 
     return render(request, 'maintenance/upload_display_excel.html', context)
@@ -172,12 +190,99 @@ def access_training_data(request):
     
 
 def download_test_case_excel(request):
-    file_path = os.path.join(settings.MEDIA_ROOT, 'training_data', 'Template.xlsx')
-    if os.path.exists(file_path):
-        with open(file_path, 'rb') as file:
-            response = FileResponse(file)
-            response['Content-Disposition'] = 'attachment; filename=Template.xlsx'
-            return response
+    # Google Sheets URL
+    google_sheets_url = "https://docs.google.com/spreadsheets/d/1fvpAEbKl7ImZaxkihJuoAfEIUVcfxfPR/edit?usp=sharing"
+
+    # Extract file ID from URL
+    file_id = google_sheets_url.split("/")[5]
+
+    # Construct direct download link
+    direct_download_link = f"https://docs.google.com/spreadsheets/d/{file_id}/export?format=xlsx"
+
+    # Redirect to direct download link
+    return redirect(direct_download_link)
+
+
+
+
+@login_required(login_url='login')
+@user_passes_test(im_driver, login_url='/login')
+def manual_input_view(request):
+    data_to_display = None
+    error_message = None
+    column_averages = None
+    maintenance_status = None
+
+    if request.method == 'POST':
+        form = ManualInputForm(request.POST)
+        if form.is_valid():
+            try:
+                truck_select = form.cleaned_data['truck_select']
+                
+                # Convert form data to DataFrame
+                data = {field: [value] for field, value in form.cleaned_data.items() if field != 'truck_select'}
+                print(data)
+                df = pd.DataFrame(data)
+
+                # Load and preprocess training data
+                if truck_select == 'truck2':
+                    train_file_path = os.path.join(settings.MEDIA_ROOT, 'training_data', 'badData.xlsx')
+                else:
+                    train_file_path = os.path.join(settings.MEDIA_ROOT, 'training_data', 'exp2_19drivers_1car_1route.xlsx')
+                train_df = load_data(train_file_path)
+                train_preprocessed, numerical_columns_updated = preprocess_data(train_df)
+                train_imputed = handle_missing_values(train_preprocessed, numerical_columns_updated)
+
+                # Train Isolation Forest model
+                isolation_forest, _ = train_isolation_forest(train_imputed, numerical_columns_updated)
+
+                # Preprocess input data
+                df_preprocessed, numerical_columns_updated = preprocess_data(df)
+                df_imputed = handle_missing_values(df_preprocessed, numerical_columns_updated)
+
+                # Detect anomalies
+                total_anomaly_score = sum_anomaly_scores(isolation_forest, df_imputed, numerical_columns_updated)
+                input_outliers = detect_outliers(isolation_forest, df_imputed, numerical_columns_updated)
+                outlier_rows = compare_outliers(df, input_outliers)
+
+                # Calculate column averages
+                column_averages = df_imputed.mean().to_dict()
+
+                # Determine maintenance status
+                maintenance_status = determine_maintenance_status(total_anomaly_score)
+
+                # Save to the database
+                ColumnAverages.objects.create(
+                    altitude_avg=column_averages.get('ALTITUDE', 0),
+                    engine_load_avg=column_averages.get('ENGINE_LOAD', 0),
+                    barometric_pressure_avg=column_averages.get('BAROMETRIC_PRESSURE', 0),
+                    engine_coolant_temp_avg=column_averages.get('ENGINE_COOLANT_TEMP', 0),
+                    ambient_air_temp_avg=column_averages.get('AMBIENT_AIR_TEMP', 0),
+                    engine_rpm_avg=column_averages.get('ENGINE_RPM', 0),
+                    intake_manifold_pressure_avg=column_averages.get('INTAKE_MANIFOLD_PRESSURE', 0),
+                    maf_avg=column_averages.get('MAF', 0),
+                    air_intake_temp_avg=column_averages.get('AIR_INTAKE_TEMP', 0),
+                    speed_avg=column_averages.get('SPEED', 0),
+                    throttle_pos_avg=column_averages.get('THROTTLE_POS', 0),
+                    engine_runtime_avg=column_averages.get('ENGINE_RUNTIME', 0),
+                    maintenance_status=maintenance_status
+                )
+
+                # Prepare data to display in HTML format
+                data_to_display = outlier_rows.to_html()
+            except Exception as e:
+                error_message = str(e)
+                print(f"Error: {error_message}")
     else:
-        # Handle file not found error
-        return HttpResponse("File not found")
+        form = ManualInputForm()
+
+    context = {
+        'form': form,
+        'data_to_display': data_to_display,
+        'error_message': error_message,
+        'column_averages': column_averages,
+        'maintenance_status': maintenance_status,
+    }
+
+    return render(request, 'maintenance/manual_input.html', context)
+
