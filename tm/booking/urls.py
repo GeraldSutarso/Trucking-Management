6bcff493247eb339ecad68125586edf167bcfcb1
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2024-05-20 13:11:51 +0700

    Check payment status

--- tm/booking/urls.py
+++ tm/booking/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     # path('customer/booking/', views.customer_booking, name='customer_booking'),
     path('select-payment/', views.select_payment, name='select_payment'),
     path('create-payment/', views.create_payment, name='create_payment'),
-    path('payment-success/', views.payment_success, name='payment_success'),
-    path('payment-pending/', views.payment_pending, name='payment_pending'),
-    path('payment_status/', views.payment_status, name='payment_status'),
+    path('payment-success/<int:booking_id>/', views.payment_success, name='payment_success'),
+    path('payment-pending/<int:booking_id>/', views.payment_pending, name='payment_pending'),
+    path('payment-declined/<int:booking_id>/', views.payment_declined, name='payment_declined'),
+    path('payment_status/<int:booking_id>/', views.payment_status, name='payment_status'),
 ]
